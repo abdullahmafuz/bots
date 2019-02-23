bf30e61d53aadfc6c2ba50b8cef1e56af63ce9de
Abdullah <47503687+abdullahmafuz@users.noreply.github.com>
2019-02-24 00:25:42 +0400

    Updates

---
+++ precache-manifest.fc8356cf79a8318409d5062ea9ef4229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49d818353a855cb6e9ed",
+    "url": "/bots/static/js/runtime~main.49d81835.js"
+  },
+  {
+    "revision": "47e0adfb5edf111c6e8b",
+    "url": "/bots/static/js/main.47e0adfb.chunk.js"
+  },
+  {
+    "revision": "55f3a3cc2ae7f76e8fb4",
+    "url": "/bots/static/js/2.55f3a3cc.chunk.js"
+  },
+  {
+    "revision": "47e0adfb5edf111c6e8b",
+    "url": "/bots/static/css/main.411ee374.chunk.css"
+  },
+  {
+    "revision": "55f3a3cc2ae7f76e8fb4",
+    "url": "/bots/static/css/2.c938a049.chunk.css"
+  },
+  {
+    "revision": "0877126e76953a92e98b38680b4ea3d1",
+    "url": "/bots/index.html"
+  }
+];
